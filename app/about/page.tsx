e8f225f841fa249c8fb50be0101d04cf21cd2aaa
Flyan <guilhermeflyan.22@gmail.com>
2026-01-18 18:16:55 -0300

    About Us Page for Desktop Devices
    I've implemented About Us Page for desktop devices.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,57 +9,68 @@ export default function About() {
             {/* SOBRE */}
             <section className="bg-[#07090A] w-full py-5 lg:py-10 pb-8 px-7 lg:px-10 flex flex-col justify-center items-center">
                 <div className="min-h-[100px] w-full"></div>
-                <h1 className="text-[#DAA520] text-[15px] lg:text-[22px] lg:text-start px-7 lg:px-10">SOBRE NÓS</h1>
-                <h2 className="text-white text-[23px] font-bold">Nossa História</h2>
+                <h1 className="text-[#DAA520] text-[15px] lg:text-[37px] lg:text-start px-7 lg:px-10">SOBRE NÓS</h1>
+                <h2 className="text-white text-[23px] lg:text-[67px] font-bold">Nossa História</h2>
                 <img src="/line.svg" alt="Line Icon" height={10} width={100} loading="eager" decoding="async" fetchPriority="low" className="lg:hidden mt-2"/>
-                <p className="text-center text-[12px] text-[#D8D8D8] mt-2">Transformamos espaços com soluções inteligentes, sustentáveis e humanas. Unimos engenharia e arquitetura com o cuidado que só uma família pode oferecer.</p>
+                <img src="/line.svg" alt="Line Icon" height={10} width={280} loading="eager" decoding="async" fetchPriority="low" className="hidden lg:block mt-2"/>
+                <p className="text-center text-[12px] lg:text-[22px] lg:max-w-[870px] text-[#D8D8D8] mt-2 lg:mt-10">Transformamos espaços com soluções inteligentes, sustentáveis e humanas. Unimos engenharia e arquitetura com o cuidado que só uma família pode oferecer.</p>
             </section>
 
             {/* HISTÓRIA */}
-            <section className="bg-[#07090A] w-full pt-2.5  lg:py-10 px-7 lg:px-10">
+            <section className="bg-[#07090A] w-full pt-2.5 px-7 lg:px-10 lg:max-h-[625px]">
                 <h1 className="text-[#DAA520] text-[14px] lg:text-[22px] lg:text-start">NOSSA HISTÓRIA</h1>
-                <div>
-                    <div>
-                        <h2 className="text-white text-[20px] font-bold">Do Lar Para o Mundo</h2>
+                <div className="lg:flex lg:justify-between">
+                    <div className="lg:max-w-[527.5px]">
+                        <h2 className="text-white text-[20px] lg:text-[48px] font-bold">Do Lar Para o Mundo</h2>
                         <img src="/line.svg" alt="Line Icon" height={10} width={90} loading="eager" decoding="async" fetchPriority="low" className="lg:hidden"/>
-                        <p className="text-[#D8D8D8] text-[12px] text-justify mt-5">
+                        <img src="/line.svg" alt="Line Icon" height={10} width={220} loading="eager" decoding="async" fetchPriority="low" className="hidden lg:block"/>
+                        <p className="lg:hidden text-[#D8D8D8] text-[12px] text-justify mt-5">
                             Lorem ipsum dolor sit amet. Cum libero obcaecati sit maxime architecto rem dolor magnam est dolores maxime non rerum molestiae. Sit dolores quaerat ut enim tempore sit accusamus repellendus.
                             <br />
                             Sed reprehenderit galisum aut assumenda omnis ut vero quia ut tempore deserunt ut enim enim. Et consequuntur repellat sed quisquam itaque et galisum sunt eos sunt quae 33 autem optio.
                         </p>
+                        <p className="hidden lg:block text-[#D8D8D8] text-[15px] text-justify mt-5">
+                            Lorem ipsum dolor sit amet. Cum libero obcaecati sit maxime architecto rem dolor magnam est dolores maxime non rerum molestiae. Sit dolores quaerat ut enim tempore sit accusamus repellendus.
+                            <br />
+                            Sed reprehenderit galisum aut assumenda omnis ut vero quia ut tempore deserunt ut enim enim. Et consequuntur repellat sed quisquam itaque et galisum sunt eos sunt quae 33 autem optio.
+                            Hic sequi molestiae et corrupti repudiandae qui nostrum quibusdam eum dolorem consectetur vel quae consequatur qui aspernatur magni. Non enim minima ad inventore esse et molestiae voluptatibus ex itaque illum eum mollitia placeat ut dolorum vitae hic iusto rerum. Ad commodi ullam et dolores quibusdam nam amet sunt non explicabo minima.
+                        </p>
                     </div>
                     <div className="flex flex-col justify-center items-center min-h-[250px] mt-5">
-                        <Image src="/office.webp" alt="Office Image" width={220} height={220} priority fetchPriority="high" className="z-10" />
-                        <img src="/square.svg" alt="Square Icon" width={86} height={86} className="relative left-[85px] bottom-[70px] z-0"/>
+                        <Image src="/office.webp" alt="Office Image" width={220} height={220} priority fetchPriority="high" className="z-10 lg:hidden" />
+                        <Image src="/office.webp" alt="Office Image" width={420} height={420} priority fetchPriority="high" className="z-10 hidden lg:block mr-10" />
+                        <img src="/square.svg" alt="Square Icon" width={86} height={86} className="relative left-[85px] bottom-[70px] z-0 lg:hidden"/>
+                        <img src="/square.svg" alt="Square Icon" width={160} height={160} className="relative left-[145px] bottom-[125px] z-0 hidden lg:block"/>
                     </div>
                 </div>
-                <div className="flex gap-5 justify-center items-center relative bottom-[40px]">
+                <div className="flex gap-5 lg:gap-[30px] justify-center items-center relative bottom-[40px] lg:justify-start lg:relative lg:bottom-[190px]  lg:max-w-[527.5px]">
                     <HistoryCard title="MISSÃO" text="Transformar espaços com soluções inteligentes, sustentáveis e humanas."/>
                     <HistoryCard title="VISÃO" text="Ser referência em arquitetura, engenharia e automação em Brasília. "/>
                 </div>
             </section>
 
             {/* VALORES */}
-            <section className="bg-[#010101] w-full lg:py-10 pb-5 flex flex-col justify-center items-center px-7 lg:px-10">
-                <h1 className="text-[#DAA520] text-[15px] lg:text-[22px] lg:text-start px-7 lg:px-10">NOSSOS VALORES</h1>
-                <h2 className="text-white text-[23px] font-bold">O Que Nos Guia</h2>
+            <section className="bg-[#010101] w-full lg:pb-10 pb-5 flex flex-col justify-center items-center px-7 lg:px-10">
+                <h1 className="text-[#DAA520] text-[15px] lg:text-[37px] lg:text-start px-7 lg:px-10">NOSSOS VALORES</h1>
+                <h2 className="text-white text-[23px] lg:text-[67px] font-bold">O Que Nos Guia</h2>
                 <img src="/line.svg" alt="Line Icon" height={10} width={100} loading="eager" decoding="async" fetchPriority="low" className="lg:hidden mt-2"/>
-                <div className="flex flex-wrap mt-5 justify-center items-center gap-5">
-                    <ValueCard title="Sustentabilidade e Consciência Energética" text="Trabalhamos com responsabilidade ambiental e soluções que visam o futuro." imgSrc="/leaf.svg" imgAlt="Leaf Icon" imgWidth={15} imgHeight={15}/>
-                    <ValueCard title="Tecnologia e Inovação" text="Incorporamos o que há de mais moderno em automação para facilitar e transformar vidas." imgSrc="/chip.svg" imgAlt="Chip Icon" imgWidth={17.5} imgHeight={17.5}/>
-                    <ValueCard title="Pessoalidade e Acolhimento" text="Cuidamos de cada projeto como quem cuida de casa - com escuta, empatia e proximidade." imgSrc="/heart.svg" imgAlt="Heart Icon" imgWidth={15} imgHeight={15}/>
-                    <ValueCard title="Comprometimento e Entrega" text="Honramos cada etapa do processo com seriedade, ética e dedicação." imgSrc="/check.svg" imgAlt="Check Icon" imgWidth={15} imgHeight={15}/>
-                    <ValueCard title="Família com Essência" text="Nosso diferencial é a raiz afetiva que orienta nossas decisões e fortalece cada relação construída." imgSrc="/people.svg" imgAlt="People Icon" imgWidth={15} imgHeight={15}/>
+                <img src="/line.svg" alt="Line Icon" height={10} width={280} loading="eager" decoding="async" fetchPriority="low" className="hidden lg:block mt-2"/>
+                <div className="flex flex-wrap mt-5 lg:mt-10 justify-center items-center gap-5 lg:max-w-[1100px]">
+                    <ValueCard title="Sustentabilidade e Consciência Energética" text="Trabalhamos com responsabilidade ambiental e soluções que visam o futuro." imgSrc="/leaf.svg" imgAlt="Leaf Icon" mobileImgWidth={15} mobileImgHeight={15} dekstopImgWidth={27.5} dekstopImgHeight={27.5}/>
+                    <ValueCard title="Tecnologia e Inovação" text="Incorporamos o que há de mais moderno em automação para facilitar e transformar vidas." imgSrc="/chip.svg" imgAlt="Chip Icon" mobileImgWidth={17.5} mobileImgHeight={17.5} dekstopImgWidth={30} dekstopImgHeight={30}/>
+                    <ValueCard title="Pessoalidade e Acolhimento" text="Cuidamos de cada projeto como quem cuida de casa - com escuta, empatia e proximidade." imgSrc="/heart.svg" imgAlt="Heart Icon" mobileImgWidth={15} mobileImgHeight={15} dekstopImgWidth={30} dekstopImgHeight={30}/>
+                    <ValueCard title="Comprometimento e Entrega" text="Honramos cada etapa do processo com seriedade, ética e dedicação." imgSrc="/check.svg" imgAlt="Check Icon" mobileImgWidth={15} mobileImgHeight={15} dekstopImgWidth={30} dekstopImgHeight={30}/>
+                    <ValueCard title="Família com Essência" text="Nosso diferencial é a raiz afetiva que orienta nossas decisões e fortalece cada relação construída." imgSrc="/people.svg" imgAlt="People Icon" mobileImgWidth={15} mobileImgHeight={15} dekstopImgWidth={30} dekstopImgHeight={30}/>
                 </div>
             </section>
 
             {/* SERVIÇOS */}
-            <section className="bg-[#06090B] w-full pb-5 lg:py-10 flex flex-col justify-center items-center px-7 lg:px-10">
-                <h1 className="text-white text-[20px] font-bold">Conheça Nossos Serviços</h1>
-                <p className="text-center text-[12px] text-[#D8D8D8] mt-5">Descubra como podemos transformar seu projeto em realidade com nossas soluções integradas.</p>
+            <section className="bg-[#06090B] w-full pb-5 lg:pb-10 flex flex-col justify-center items-center px-7 lg:px-10">
+                <h1 className="text-white text-[20px] lg:text-[33px] font-bold">Conheça Nossos Serviços</h1>
+                <p className="text-center text-[12px] lg:text-[22px] lg:max-w-[870px] text-[#D8D8D8] mt-5">Descubra como podemos transformar seu projeto em realidade com nossas soluções integradas.</p>
                 <div className="flex justify-between mt-6 gap-5">
-                    <Link href="/services" className="font-bold bg-[#DAA520] text-[15px] py-1 px-2.5 cursor-pointer">VER SERVIÇOS</Link>
-                    <Link href="/#contact" className="text-[#DAA520] text-[17px] flex gap-2 cursor-pointer">
+                    <Link href="/services" className="font-bold bg-[#DAA520] text-[15px] lg:text-[19px] py-1 px-2.5 lg:px-4 transition hover:scale-105 cursor-pointer">VER SERVIÇOS</Link>
+                    <Link href="/#contact" className="text-[#DAA520] text-[17px] lg:text-[21px] flex gap-2 transition hover:scale-105 cursor-pointer">
                         Fale Conosco
                         <img src="/arrow.svg" alt="Arrow Icon" width={15} height={10} loading="eager" decoding="async" fetchPriority="low" className="mb-0.5"/>
                     </Link>
--- components/aboutCards.tsx
+++ components/aboutCards.tsx
@@ -5,9 +5,9 @@ type HistoryCardProps = {
 
 export function HistoryCard({title, text}: HistoryCardProps) {
     return(
-        <article className="p-2.5 ring-[0.15px] ring-[#D8D8D8] bg-[#0E1011] w-[142px] rounded-sm">
-            <h1 className="font-bold text-[9px] text-[#DAA520]">{title}</h1>
-            <h2 className="text-[9px] text-[#D8D8D8] mt-1">{text}</h2>
+        <article className="p-2.5 ring-[0.15px] ring-[#D8D8D8] bg-[#0E1011] w-[142px] lg:w-[250px] rounded-sm">
+            <h1 className="font-bold text-[9px] lg:text-[13px] text-[#DAA520]">{title}</h1>
+            <h2 className="text-[9px] lg:text-[13px] text-[#D8D8D8] mt-1">{text}</h2>
         </article>
     );
 }
@@ -17,16 +17,19 @@ type ValueCardProps = {
     text: string,
     imgSrc: string,
     imgAlt: string,
-    imgWidth: number,
-    imgHeight: number;
+    mobileImgWidth: number,
+    mobileImgHeight: number,
+    dekstopImgWidth: number,
+    dekstopImgHeight: number;
 }
 
-export function ValueCard({title, text, imgSrc, imgAlt, imgWidth, imgHeight}: ValueCardProps) {
+export function ValueCard({title, text, imgSrc, imgAlt, mobileImgWidth, mobileImgHeight, dekstopImgWidth, dekstopImgHeight}: ValueCardProps) {
     return(
-        <article className="p-2.5 ring-[0.15px] ring-[#D8D8D8] bg-[#0E1011] rounded-sm w-[142px] h-[100px]">
-            <img src={imgSrc} alt={imgAlt} width={imgWidth} height={imgHeight} loading="eager" decoding="async" fetchPriority="low" />
-            <h1 className="font-bold text-[7px] text-[#D8D8D8] mt-2">{title}</h1>    
-            <h2 className="text-[6px] text-[#D8D8D8] mt-2">{text}</h2>
+        <article className="p-2.5 lg:p-7 ring-[0.15px] ring-[#D8D8D8] bg-[#0E1011] rounded-sm w-[142px] lg:w-[325px] h-[100px] lg:h-[185px]">
+            <img src={imgSrc} alt={imgAlt} width={mobileImgWidth} height={mobileImgHeight} loading="eager" decoding="async" fetchPriority="low" className="lg:hidden"/>
+            <img src={imgSrc} alt={imgAlt} width={dekstopImgWidth} height={dekstopImgHeight} loading="eager" decoding="async" fetchPriority="low" className="hidden lg:block"/>
+            <h1 className="font-bold text-[7px] lg:text-[14px] text-[#D8D8D8] mt-2">{title}</h1>    
+            <h2 className="text-[6px] lg:text-[13px] text-[#D8D8D8] mt-2">{text}</h2>
         </article>
     );
 
