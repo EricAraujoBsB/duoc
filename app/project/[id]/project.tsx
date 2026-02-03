b0bb11eae9f8947af779ae3e6e0dfbf1ac7cbda0
Flyan <guilhermeflyan.22@gmail.com>
2026-02-03 16:18:07 -0300

    up

--- app/project/[id]/project.tsx
+++ app/project/[id]/project.tsx
@@ -18,12 +18,12 @@ export default async function Project({params}: {params: Promise<{id: string}>})
                     <h2 className="text-left text-[12px] md:text-[18px] lg:text-[22px] text-[#D8D8D8] mt-2 max-w-[1456px]">{project.descricao}</h2>
                     <img src="/line.svg" alt="Line Icon" height={10} width={130} loading="eager" decoding="async" fetchPriority="low" className="lg:hidden mt-5"/>
                     <img src="/line.svg" alt="Line Icon" height={10} width={250} loading="eager" decoding="async" fetchPriority="low" className="hidden lg:block mt-7"/>
-                    <div className="w-full flex justify-center items-center">
-                        <div className="flex flex-col justify-center items-center xl:flex-row xl:items-start xl:gap-10 w-full">
-                            <div className="w-full">
-                                <ProjectCarousel info_imagem={project.info_imagem}/>
-                            </div>
-                            <article className="bg-[#0E1011] border border-[#1F1F1F] rounded-xl p-5 md:p-7 lg:p-10 w-full max-w-[375px] md:max-w-[500px] lg:max-w-[600px] lg:w-full lg:min-h-[250px] lg:max-h-[250px] xl:min-w-[420px] xl:max-w-full mt-5 lg:mt-10">
+                    <div className="flex flex-col justify-center items-center xl:gap-10 w-full">
+                        <div className="w-full">
+                            <ProjectCarousel info_imagem={project.info_imagem}/>
+                        </div>
+                        <div className="flex flex-col xl:flex-row w-full xl:gap-10"> 
+                            <article className="bg-[#0E1011] border border-[#1F1F1F] rounded-xl p-5 md:p-7 lg:p-10 w-full  mt-5 lg:mt-10">
                                 <h1 className="font-bold text-[14px] md:text-[18px] lg:text-[22px]">Informações do Projeto</h1>
                                 <ul className="text-[12px] m-5 flex flex-col gap-3  md:text-[15px] lg:text-[18px] lg:gap-2">
                                     <li className="flex gap-2">
@@ -41,6 +41,14 @@ export default async function Project({params}: {params: Promise<{id: string}>})
                                         <img src="/clock-project.svg" alt="Duration Icon" width={22} height={22} loading="eager" decoding="async" fetchPriority="low" className="hidden xl:block"/>
                                         Duracao: {project.duracao}
                                     </li>
+                                </ul>
+                            </article>
+                            <article className="bg-[#0E1011] border border-[#1F1F1F] rounded-xl p-5 md:p-7 lg:p-10 w-full mt-5 lg:mt-10">
+                                <h1 className="font-bold text-[14px] md:text-[18px] lg:text-[22px]">Serviço Relacionado</h1>
+                                <ul className="text-[12px] m-5 flex flex-col gap-3  md:text-[15px] lg:text-[18px] lg:gap-2">
+                                        {project.servico_nome.map((servico, index)=>(
+                                            <li key={index}>{servico}</li>
+                                        ))}
                                 </ul>
                             </article>
                         </div>
